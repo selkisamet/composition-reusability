26b99ab36338058a1109d8487d7bca4f5b92e7d6
Samet <sametselki@outlook.com>
2024-03-18 23:36:34 +0300

    accordion icon component added decsription complete

--- src/components/AccordionContent.jsx
+++ src/components/AccordionContent.jsx
@@ -1,7 +1,7 @@
 import { React, useContext } from "react";
 import { AccordionContext } from "./accordion";
 
-// AccordionContent adında bir fonksiyon bileşeni oluşturur ve bu bileşen, children adında bir prop alır
+// AccordionContent adında bir fonksiyon bileşeni oluşturur ve bu bileşen, children adında bir prop alır.
 const AccordionContent = ({ children }) => {
     // AccordionContext'ten expanded adlı değişkeni alır.
     const { expanded } = useContext(AccordionContext);
--- src/components/AccordionIcon.jsx
+++ src/components/AccordionIcon.jsx
@@ -1,10 +1,13 @@
 import { React, useContext } from "react";
 import { AccordionContext } from "./accordion";
 
+// AccordionIcon adında bir fonksiyon bileşenini tanımlar.
+// Varsayılan olarak opened ve closed parametreleri alır.
 const AccordionIcon = ({ opened = "-", closed = "-" }) => {
+    // AccordionContext'ten expanded adlı değişkeni alır.
     const { expanded } = useContext(AccordionContext);
 
-    return <span>{expanded ? opened : closed}</span>
+    // Eğer expanded true ise opened değerini, değilse closed değerini döndürür.    return <span>{expanded ? opened : closed}</span>
 }
 
 export default AccordionIcon;
